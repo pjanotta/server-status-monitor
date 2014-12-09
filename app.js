0aef68263f6b6a14476f50d20b0949dd71df0acb
Peter Janotta <git.xfvz@mapiverse.de.vu>
2014-12-09 23:05:08 +0100

    fix app.js

--- app.js
+++ app.js
@@ -4,8 +4,6 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var fs = require('fs');
@@ -38,8 +36,6 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
