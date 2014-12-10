2fd062dd9cfdcc9f7c9cbf0e9aff8c76f6eee868
Peter Janotta <git.xfvz@mapiverse.de.vu>
2014-12-10 10:05:20 +0100

    Changed indentations to spaces to fix code layout

--- app.js
+++ app.js
@@ -19,15 +19,15 @@ var serverSchema = mongoose.Schema({
 var Server = mongoose.model('Server', serverSchema);
 
 function getMongooseConnection(dbAddress, callback) {
-	var db;
+    var db;
     mongoose.connect(dbAddress);
 
     db = mongoose.connection;
     db.on('error', function (err) {
-    	console.error('DB connection error: ' + err);
-    	console.error('closing DB connection');
-    	mongoose.disconnect();
-    	return;
+        console.error('DB connection error: ' + err);
+        console.error('closing DB connection');
+        mongoose.disconnect();
+        return;
     });
     db.once('open', callback);
 }
@@ -46,164 +46,164 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  	app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
 
 
 app.get('/config/servers.json', function (req, res) {
-	getMongooseConnection('mongodb://localhost/servers', function () {
+    getMongooseConnection('mongodb://localhost/servers', function () {
         Server.find({}, function(err, data) {
-        	mongoose.disconnect();
-        	if(!err) {
-        		res.json(data);
-        	} else {
-        		res.send(500);
-        	}
-    	});
-	});	
+            mongoose.disconnect();
+            if(!err) {
+                res.json(data);
+            } else {
+                res.send(500);
+            }
+        });
+    }); 
 });
 
 
 app.get('/test', function (req, res) {
-	setTimeout(function () {
-		res.send(500);
-	}, 700);
+    setTimeout(function () {
+        res.send(500);
+    }, 700);
 });
 
 
 app.delete('/remove/:id', function(req, res) {
     if(!req.params) res.send(400); //Bad request
-	getMongooseConnection('mongodb://localhost/servers', function () {
+    getMongooseConnection('mongodb://localhost/servers', function () {
         Server.remove({"_id": new ObjectId(req.params.id)}, function(err) {
-        	mongoose.disconnect();
-        	if(!err) {
-        		res.send(200);
-        	} else {
-        		res.send(500, 'Error removing server with _id: ' );
-        	}
-    	});
-	});
+            mongoose.disconnect();
+            if(!err) {
+                res.send(200);
+            } else {
+                res.send(500, 'Error removing server with _id: ' );
+            }
+        });
+    });
 });
 
 
 app.put('/modify/:id', function(req, res) {
     if(!req.body || !req.params) res.send(400); //Bad request
-	getMongooseConnection('mongodb://localhost/servers', function () {
+    getMongooseConnection('mongodb://localhost/servers', function () {
        var newProperties = {
-        	"description":req.body.description || '',
-        	"host":req.body.host || '',
-        	"port":req.body.port || 80,
-        	"url":req.body.url || '',
-        	"CORS": !!req.body.CORS
+            "description":req.body.description || '',
+            "host":req.body.host || '',
+            "port":req.body.port || 80,
+            "url":req.body.url || '',
+            "CORS": !!req.body.CORS
         };
         console.log(newProperties);
         Server.update({"_id": new ObjectId(req.params.id)}, newProperties, { upsert: true }, function(err) {
-        	mongoose.disconnect();
-        	if(!err) {
-        		res.send(200, 'Successful update of #' + req.params.id);
-        	} else {
-        		res.send(500, 'Error updating infos for server with _id: ' + req.params.id);
-        	}
-    	});
-	});
+            mongoose.disconnect();
+            if(!err) {
+                res.send(200, 'Successful update of #' + req.params.id);
+            } else {
+                res.send(500, 'Error updating infos for server with _id: ' + req.params.id);
+            }
+        });
+    });
 });
 
 
 app.post('/add', function (req, res) {
     console.log('add');
     if(!req.body) res.send(400); //Bad request
-	getMongooseConnection('mongodb://localhost/servers', function () {
+    getMongooseConnection('mongodb://localhost/servers', function () {
         var newServer = new Server({
-        	"description":req.body.description || '',
-        	"host":req.body.host || '',
-        	"port":req.body.port || 80,
-        	"url":req.body.url || '',
-        	"CORS": !!req.body.CORS
+            "description":req.body.description || '',
+            "host":req.body.host || '',
+            "port":req.body.port || 80,
+            "url":req.body.url || '',
+            "CORS": !!req.body.CORS
         });
         console.log('adding server');
         console.log(newServer);
         newServer.save(function(err, addedServer) {
-        	mongoose.disconnect();
-        	if(!err) {
-        		console.log('added');
-        		res.json(addedServer);
-        	} else {
-        		console.log('error');
-        		res.send(500, 'Error while adding server');
-        	}
-    	});
-	});
+            mongoose.disconnect();
+            if(!err) {
+                console.log('added');
+                res.json(addedServer);
+            } else {
+                console.log('error');
+                res.send(500, 'Error while adding server');
+            }
+        });
+    });
 });
 
 
 app.post('/restore-defaults', function (req, res) {
-	getMongooseConnection('mongodb://localhost/servers', function () {
-	    Server.remove({}, function (err) {
-	        if (err) { 
-	            console.error(err);
-	            console.error('disconnect + abort'); 
-	            mongoose.disconnect();
-	            res.send(500);
-	        } else { 
-	            console.log('Old server config cleared from DB');
-	            copyJSON();              
-	        }
-	    });
+    getMongooseConnection('mongodb://localhost/servers', function () {
+        Server.remove({}, function (err) {
+            if (err) { 
+                console.error(err);
+                console.error('disconnect + abort'); 
+                mongoose.disconnect();
+                res.send(500);
+            } else { 
+                console.log('Old server config cleared from DB');
+                copyJSON();              
+            }
+        });
 
-	    function copyJSON() {
-	        var servers;
-	        console.log('Copy config/servers-default.json to servers DB');
-	        servers = JSON.parse(fs.readFileSync('config/servers-default.json'));
+        function copyJSON() {
+            var servers;
+            console.log('Copy config/servers-default.json to servers DB');
+            servers = JSON.parse(fs.readFileSync('config/servers-default.json'));
 
-	        async.map(servers, saveServer, function (err, results) {
-	            mongoose.disconnect();
-	            res.json(results);
-	        });
+            async.map(servers, saveServer, function (err, results) {
+                mongoose.disconnect();
+                res.json(results);
+            });
 
-	        function saveServer(item, callback) {
-	            var newServer;
-	            newServer = new Server(item);
-	            newServer.save(function(err, savedServer) {
-	                if (err) { 
-	                    console.error(err); 
-	                    res.send(500);
-	                } else { 
-	                    console.log('saved server with _id: ' + savedServer["_id"]); 
-	                }
-	                callback(err, savedServer);
-	            });
-	        }
-	    }
-	});
+            function saveServer(item, callback) {
+                var newServer;
+                newServer = new Server(item);
+                newServer.save(function(err, savedServer) {
+                    if (err) { 
+                        console.error(err); 
+                        res.send(500);
+                    } else { 
+                        console.log('saved server with _id: ' + savedServer["_id"]); 
+                    }
+                    callback(err, savedServer);
+                });
+            }
+        }
+    });
 });
 
 
 // Route through server in case of no CORS
 app.get('/check', function(req, res) {
-	if(req.query && req.query.q) {
-		console.log('check: ' + decodeURIComponent(req.query.q))
-		request(decodeURIComponent(req.query.q), function (err, newRes, body) {
-        	if(!err && newRes && newRes.statusCode === 200) {
-        		if(newRes.headers && newRes.headers['content-type'] && newRes.headers['content-type'].indexOf('application/json') != -1) {
-        			res.json(JSON.parse(body));
-        		} else {
-        			res.send(200);
-        		}
-        	} else {
-        		if(err) console.error(err);
-                if(newRes && newRes.statusCode && body) {
-        			res.send(newRes.statusCode, body);
+    if(req.query && req.query.q) {
+        console.log('check: ' + decodeURIComponent(req.query.q))
+        request(decodeURIComponent(req.query.q), function (err, newRes, body) {
+            if(!err && newRes && newRes.statusCode === 200) {
+                if(newRes.headers && newRes.headers['content-type'] && newRes.headers['content-type'].indexOf('application/json') != -1) {
+                    res.json(JSON.parse(body));
                 } else {
-                	res.send(500);
+                    res.send(200);
                 }
-        	}
-		});
-	} else {
+            } else {
+                if(err) console.error(err);
+                if(newRes && newRes.statusCode && body) {
+                    res.send(newRes.statusCode, body);
+                } else {
+                    res.send(500);
+                }
+            }
+        });
+    } else {
         //Bad request
-		res.send(400);
-	}
+        res.send(400);
+    }
 });
 
 http.createServer(app).listen(app.get('port'), function(){
-  	console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port ' + app.get('port'));
 });
